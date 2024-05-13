6dcfd330b6ca4a8a75bdb09f0b01cafa0d95ffb5
zhin abubakr <zheen99abubakr@gmail.com>
2024-05-14 00:49:20 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, query) {
+  const lowercaseQuery = query.toLowerCase();
+  return drivers.filter((driver) =>
+    driver.toLowerCase().includes(lowercaseQuery)
+  );
+}
+
+function fuzzyMatch(drivers, query) {
+  const lowercaseQuery = query.toLowerCase();
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(lowercaseQuery)
+  );
+}
+
+function matchName(driverObjects, query) {
+  const lowercaseQuery = query.toLowerCase();
+  return driverObjects.filter(
+    (driver) => driver.name.toLowerCase() === lowercaseQuery
+  );
+}
